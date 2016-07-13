edde321c64a09ed0b701410bd5ecb48a54027522
JulianSansat <kerarchie@yahoo.com.ar>
2016-07-12 22:37:57 -0300

    Allow cors

--- config/application.rb
+++ config/application.rb
@@ -33,6 +33,10 @@ module Reino
         :authentication => :login,
     }
 
+    config.action_dispatch.default_headers.merge!({
+        'Access-Control-Allow-Origin' => '*',
+        'Access-Control-Request-Method' => '*'
+    })
 
   end
 end
