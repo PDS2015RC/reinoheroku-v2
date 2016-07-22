b20aeb112c4c8c402326b28e0cc76ff38a72c64b
Renato Soares <renatosoarespro@hotmail.com>
2016-07-22 10:34:01 -0300

    estilos funcionando e form ok

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -8,15 +8,19 @@ Rails.application.config.assets.version = '1.0'
 
 # Precompile additional assets.
 # application.js, application.css, and all non-JS/CSS in app/assets folder are already added.
-Rails.application.config.assets.precompile += %w( search.js )
-Rails.application.config.assets.precompile += %w( bootstrap.css )
-Rails.application.config.assets.precompile += %w( bootstrap.min.js )
+# Rails.application.config.assets.precompile += %w( search.js )
+# Rails.application.config.assets.precompile += %w( bootstrap.css )
+# Rails.application.config.assets.precompile += %w( bootstrap.min.js )
+
+Rails.application.config.assets.precompile += %w( vendor/revslider/rs-plugin/css/settings.css )
 Rails.application.config.assets.precompile += %w( __packed.css )
 Rails.application.config.assets.precompile += %w( fontello/css/fontello.css )
 Rails.application.config.assets.precompile += %w( fontello/css/animation.css )
-Rails.application.config.assets.precompile += %w( styles.css )
+Rails.application.config.assets.precompile += %w( style.css )
 Rails.application.config.assets.precompile += %w( shortcodes.css )
+Rails.application.config.assets.precompile += %w( kidscare.css )
 Rails.application.config.assets.precompile += %w( responsive.css )
+Rails.application.config.assets.precompile += %w( novo_reino.css )
 Rails.application.config.assets.precompile += %w( vendor/jquery-1.11.3.min.js )
 Rails.application.config.assets.precompile += %w( vendor/jquery-migrate.min.js )
 Rails.application.config.assets.precompile += %w( vendor/revslider/rs-plugin/js/jquery.themepunch.tools.min.js )
